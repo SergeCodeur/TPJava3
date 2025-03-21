1e6104b115abe90907026a514b90d2cd47557257
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2025-03-21 19:37:40 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Entrez un nombre entier : ");
+            int nombre = scanner.nextInt();
+
+            System.out.print("Divisez " + nombre + " par combien ? ");
+            int diviseur = scanner.nextInt();
+
+            int resultat = nombre / diviseur;
+            System.out.println("Résultat : " + resultat);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Erreur : Division par zéro interdite !");
+        } catch (Exception e) {
+            System.out.println("Erreur : Entrée invalide, veuillez entrer un nombre entier.");
+        } finally {
+            System.out.println("Fin du programme. Merci d'avoir testé !");
+        }
+    }
+}
